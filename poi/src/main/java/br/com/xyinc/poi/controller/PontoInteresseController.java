36f77c22458262f3d44fad9d88003d5a6fb8abae
Hanna Barbosa <hannacarolinabarbosa@gmail.com>
2017-05-12 10:55:58 -0300

    Correção para client Angular consultar back-end

--- poi/src/main/java/br/com/xyinc/poi/controller/PontoInteresseController.java
+++ poi/src/main/java/br/com/xyinc/poi/controller/PontoInteresseController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import br.com.xyinc.poi.service.IPontoInteresseService;
 /**
  * Controlador de requisições para {@link PontoInteresse}
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/pontoInteresse")
 public class PontoInteresseController {
